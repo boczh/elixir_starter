d1693cec100cab46ed0ed038b3515894c0a2d1c3
Hunor-Chris Bocz <h.bocz@elsevier.com>
2021-09-03 12:55:33 +0000

    functions

--- lib/example.ex
+++ lib/example.ex
@@ -16,4 +16,23 @@ defmodule Example do
   def fibonacci(1), do: 1
   def fibonacci(n) when n>1, do: fibonacci(n-1) + fibonacci(n-2)
 # When n>1 is a Guard
+
+@doc """
+  Calculate areas of different shapes
+  iex> Example.area(:ellipse, 2, 3)
+  18.84955592153876
+  iex> Example.area(:rectangle,2,3)
+  6
+  iex> Example.area(:triangle,2,3)
+  3.0
+  """
+  def area(:ellipse, a, b) do
+  :math.pi * a * b
+  end
+  def area(:rectangle, a, b) do
+    a * b
+  end
+  def area(:triangle, a, b) do
+    0.5 * a * b
+  end
 end
